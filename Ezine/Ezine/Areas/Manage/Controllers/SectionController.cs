08b2d91f79d14e7cc3bef4f8823106a375a8645e
jreey <qdw8911@sina.com>
2014-09-19 17:50:07 +0800

    add section

--- Ezine/Ezine/Areas/Manage/Controllers/SectionController.cs
+++ Ezine/Ezine/Areas/Manage/Controllers/SectionController.cs
@@ -3,15 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ezine.Abstract;
+using Ezine.Models;
 
 namespace Ezine.Areas.Manage.Controllers
 {
     public class SectionController : Controller
     {
+        private IEzine ezineRepository;
+        private ISection sectionRepository;
+        public SectionController(IEzine ezine, ISection section)
+        {
+            ezineRepository = ezine;
+            sectionRepository = section;
+        }
         // GET: Manage/Section
         public ActionResult Index()
         {
             return View();
+        }
+
+        /// <summary>
+        /// 添加目录
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AddSections(int id)
+        {
+            var ezine = ezineRepository.GetEzine(id);
+            return View(ezine);
+        }
+
+        /// <summary>
+        /// 添加目录
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult Save(Section model)
+        {
+            return Json("test");
         }
     }
 }
--- Ezine/Ezine/Scripts/controllers.js
+++ Ezine/Ezine/Scripts/controllers.js
@@ -1,28 +1,71 @@
-﻿var ezineController = angular.module('ezineController', []);
+﻿(function () {
 
-//杂志封面
-ezineController.controller('ezineList', function ($scope, $http) {
-    var ezine = $http({
-        method: 'GET',
-        url: '/Ezine/EzineList'
+    var ezineController = angular.module('ezineController', []);
+
+    //杂志封面
+    ezineController.controller('ezineList', function ($scope, $http) {
+        GetEzineData($scope, $http);
+        //删除
+        $scope.delEzine = function (obj) {
+            var del = $http({
+                method: 'GET',
+                url: '/Ezine/Delete',
+                params: { 'id': obj.EzineId }
+            });
+            del.success(function (response, status, headers, config) {
+                $scope.result = response;
+                if (response === 'true') {
+                    GetEzineData($scope, $http);
+                }
+            });
+        }
     });
-    ezine.success(function (response, status, headers, config) {
-        $scope.ezines = response;
-    });
 
-    //编辑
-
-    //删除
-    $scope.delEzine = function (obj) {
-        console.info(obj.EzineId);
-        var del = $http({
+    //获取所欲数据
+    function GetEzineData($scope, $http) {
+        var ezine = $http({
             method: 'GET',
-            url: '/Ezine/Delete',
-            params: { 'id': obj.EzineId }
+            url: '/Ezine/EzineList'
         });
-        del.success(function (response, status, headers, config) {
-            $scope.result = response;
+        ezine.success(function (response, status, headers, config) {
+            $scope.ezines = response;
         });
     }
 
-});
+    // 杂志章节
+    sectionController.inject = ['$scope', '$http']; // 依赖注入
+    ezineController.controller('SectionCtrl', sectionController);
+
+    function sectionController($scope, $http) {
+        // 初始化
+        $scope.sections = [
+            {
+                id: '1',
+                placeholder: '章节名称'
+            }
+        ];
+
+        // 添加章节
+        $scope.addChapter = function () {
+            var sectionsNum = $scope.sections.length;
+            var newSection = {
+                id: sectionsNum + 1,
+                placeholder: '章节名称'
+            };
+            $scope.sections.push(newSection);
+        }
+
+        //$scope.$watch('');
+
+        //保存章节
+        $scope.saveSections = function () {
+            var save = $http({
+                method: 'POST',
+                url: '/Manage/Section/Save'
+            }).success(function (response, status, headers, config) {
+                console.info(response);
+            });
+        }
+    }
+
+})();
